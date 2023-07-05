6284216286aa925e6f9383e0a1372532513e5a78
Victor <victorzuev87@outlook.com>
2023-07-05 19:44:30 +0300

    Add time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -1,1 +1,12 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_basket_button_on_page(browser):
+    browser.get(link)
+    time.sleep(30)
+    basket_button = browser.find_element(By.ID, "add_to_basket_form")
+    assert basket_button.is_displayed()  # проверка отображения кнопки на странице
 
